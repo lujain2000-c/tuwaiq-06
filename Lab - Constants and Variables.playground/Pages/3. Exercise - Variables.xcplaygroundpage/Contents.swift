dd3eca6b97dd71e8b892fad9b14c3c827fad1303
LUJAIN ALKANHAL <lujain.i.k20@gmail.com>
2023-05-07 23:14:52 +0300

    assignment1-week2

---
+++ Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+/*:
+## Exercise - Variables
+ 
+ Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
+ */
+var schooling = 12
+
+//:  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
+schooling = 16
+print(schooling)
+//:  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
+print("Because we used var instead of let , it allows change the value")
+
+/*:
+[Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
+ */
